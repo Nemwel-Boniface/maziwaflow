506ef5294cca8c24be28a03e025798fcc553b708
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-02-17 11:07:50 +0300

    Ensure that all subsequent created tabkes use UUIDs as primary keys

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module Maziwaflow
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # Ensure that all new tables use UUIDs as primary keys by default
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
