9dd74c4d05b062233473d0604120b429dfe02c19
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-02-19 14:53:52 +0300

    Update included:   - Create the customers controller adding the needed actions for Create, view   - Update the dashboards controller to pull accurate customers information in real time   - Update the routes to include the customers name space

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
   load_and_authorize_resource class: :false
   def index
-    @total_customers = 0
-    @total_litres_today = 0
-    @debt_outstanding = 0
+    @total_customers = Customer.active.count
+    @total_liters_today = 0
+    @outstanding_debt = Customer.sum(:balance)
 
     # Fetch the total number of staff who are not the one who is loogegd in
     @staff_count = User.where.not(id: current_user.id).count
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   end
 
   get "dashboard", to: "dashboard#index", as: :dashboard
+  resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
