72c546a2fd681773a1902aeb116a65736ded3487
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-02-20 22:16:15 +0300

    Added custom pagination to the customer resource views: index, show, payments and sales

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,13 +2,19 @@ class CustomersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @customers = Customer.ordered
+    @per_page = 15
+    @page = (params[:page] || 1).to_i
+
+    all_customers = Customer.ordered
+    @total_count = all_customers.size
+    @total_pages = (@total_count / @per_page.to_f).ceil
+
+    @customers = all_customers.offset((@page - 1) * @per_page).limit(@per_page)
   end
 
   def toggle_active
     @customer = Customer.find(params[:id])
 
-    # If trying to DEACTIVATE (current is true) while they owe money
     if @customer.active && @customer.balance > 0
       redirect_to customer_path(@customer), alert: "Cannot deactivate customer with an outstanding balance of KES #{@customer.balance}."
       return
@@ -24,13 +30,23 @@ class CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
 
-    # Data to be used by the charts
+    # Chart Data
     @liters_history = @customer.sales.group_by_day(:created_at, last: 30).sum(:liters)
     @payments_history = @customer.payments.group_by_day(:created_at, last: 30).sum(:amount)
 
-    # Recent  sales and payments for the customer
-    @recent_sales = @customer.sales.order(created_at: :desc).limit(5)
-    @recent_payments = @customer.payments.order(created_at: :desc).limit(5)
+    # Paginate Sales
+    @sales_per_page = 10
+    @sales_page = (params[:sales_page] || 1).to_i
+    all_sales = @customer.sales.order(created_at: :desc)
+    @sales_total_pages = (all_sales.size / @sales_per_page.to_f).ceil
+    @recent_sales = all_sales.offset((@sales_page - 1) * @sales_per_page).limit(@sales_per_page)
+
+    # Paginate Payments
+    @payments_per_page = 10
+    @payments_page = (params[:payments_page] || 1).to_i
+    all_payments = @customer.payments.order(created_at: :desc)
+    @payments_total_pages = (all_payments.size / @payments_per_page.to_f).ceil
+    @recent_payments = all_payments.offset((@payments_page - 1) * @payments_per_page).limit(@payments_per_page)
   end
 
   def new
@@ -39,13 +55,10 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.new(customer_params)
-
-    respond_to do |format|
-      if @customer.save
-        format.html { redirect_to customers_path, notice: "Customer created successfully.", status: :see_other }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @customer.save
+      redirect_to customers_path, notice: "Customer created successfully."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
